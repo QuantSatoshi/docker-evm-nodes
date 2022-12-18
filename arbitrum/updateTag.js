7eb3d2e30166ce042eef29c6ecb515a8dcb56008
wanghao <whateverhow@gmail.com>
2022-12-18 15:52:27 -0600

    update tag

---
+++ arbitrum/updateTag.js
@@ -0,0 +1,22 @@
+const axios = require("axios");
+const fs = require('fs');
+
+async function updateTag() {
+  const res = await axios.get('https://hub.docker.com/v2/namespaces/offchainlabs/repositories/nitro-node/tags');
+  const latestTag = res.data.results[0].name;
+  console.log(`got latestTag`, latestTag);
+  if (latestTag) {
+    const fileName = __dirname + '/docker-compose.yml';
+    fs.readFile(fileName, 'utf8', function (err,data) {
+      if (err) {
+        return console.log(err);
+      }
+      const result = data.replace(/image: offchainlabs\/(.*)\n/, `image: offchainlabs/nitro-node:${latestTag}\n`);
+      fs.writeFile(fileName, result, 'utf8', function (err) {
+        if (err) return console.log(err);
+        console.log(`tag updated to ${latestTag}`);
+      });
+    });
+  }
+}
+updateTag() 
