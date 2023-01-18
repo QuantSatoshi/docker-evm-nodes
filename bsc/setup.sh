f2cd5a9e6f88a98d1c085cd1e53081571995534b
wanghao <whateverhow@gmail.com>
2023-01-18 13:54:56 -0600

    [master]   (use "git restore --staged <file>..." to unstage)

--- bsc/setup.sh
+++ bsc/setup.sh
@@ -1,7 +1,12 @@
 sudo mkdir -p /data/bsc
 sudo chown $USER:$USER /data/bsc
+mkdir -p /data/bsc/config
 
 cd ~
 git clone https://github.com/bnb-chain/bsc.git || true
 cd ~/bsc
 docker build -t bsc .
+cp ./config.toml /data/bsc/config
+cp ./genesis.json /data/bsc/config
+
+docker run -it --rm -v /data/bsc/config:/bsc/config -v /data/bsc:/data bsc geth --datadir data init genesis.json
