1bd4928707abbf713c01ca67ec16c8552f1197f2
wanghao <whateverhow@gmail.com>
2023-03-08 21:10:40 -0600

    [master]   (use "git restore --staged <file>..." to unstage)

---
+++ celo/setup.sh
@@ -0,0 +1,10 @@
+export CELO_IMAGE=us.gcr.io/celo-org/geth:mainnet
+docker pull $CELO_IMAGE
+mkdir -p /data/celo
+mkdir -p /data/celo/data
+
+cd /data/celo/data
+
+docker run -v $PWD:/root/.celo --rm -it $CELO_IMAGE account new
+
+export CELO_ACCOUNT_ADDRESS=<>
