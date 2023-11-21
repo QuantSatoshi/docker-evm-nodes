30ee263ed619b4e452268d2a43f4eaab750c6ecd
wanghao <whateverhow@gmail.com>
2023-11-21 17:19:16 -0600

    [master]   (use "git restore --staged <file>..." to unstage)

--- zkevm/setup.sh
+++ zkevm/setup.sh
@@ -7,7 +7,7 @@ cd ZKEVM_DIR
 curl -L https://github.com/0xPolygonHermez/zkevm-node/releases/latest/download/$ZKEVM_NET.zip > $ZKEVM_NET.zip && unzip -o $ZKEVM_NET.zip -d $ZKEVM_DIR && rm $ZKEVM_NET.zip
 mkdir -p $ZKEVM_CONFIG_DIR && cp $ZKEVM_DIR/$ZKEVM_NET/example.env $ZKEVM_CONFIG_DIR/.env
 
-# EDIT THIS env file:
+# EDIT THIS env file, all folder location are for local path, not docker path
 cd /data/zk-polygon/mainnet
 vim .env
 
@@ -16,3 +16,4 @@ vim .env
 cd /data/zk-polygon/mainnet
 docker compose up -d
 
+# change the default ports for docker-compose.yml to avoid port conflict
