33948568a069abcdaecdf7a54e2a3639ed6236a8
wanghao <whateverhow@gmail.com>
2024-04-13 17:29:44 -0500

    [master]   (use "git restore --staged <file>..." to unstage)

--- optimism/scripts/start-op-node.sh
+++ optimism/scripts/start-op-node.sh
@@ -16,7 +16,7 @@ fi
 # Start op-node.
 exec op-node \
   --l1=$OP_NODE__RPC_ENDPOINT \
-  --l1.beacon=http://lighthouse:5052 \
+  --l1.beacon=$OP_NODE_BEACON_ENDPOINT \
   --l2=http://op-geth:8551 \
   --rpc.addr=127.0.0.1 \
   --rpc.port=9545 \
