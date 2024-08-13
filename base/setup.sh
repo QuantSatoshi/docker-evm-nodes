91d262ea555dc8eb5b9d69a748c9b77ad9b618e0
wanghao <whateverhow@gmail.com>
2024-08-12 20:26:15 -0500

    [master]   (use "git restore --staged <file>..." to unstage)

--- base/setup.sh
+++ base/setup.sh
@@ -1,4 +1,7 @@
 git clone https://github.com/base-org/node.git base
 cd base
-docker build -t base .
+docker build -t base -f geth/Dockerfile .
 
+cd ../docker-evm-nodes/base
+
+docker compose up -d
