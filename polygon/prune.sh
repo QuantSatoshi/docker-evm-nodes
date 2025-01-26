6b42eb119b73fc58e0bc9609944da36bee2c1a67
wanghao <whateverhow@gmail.com>
2025-01-26 09:43:16 -0600

    [master]   (use "git restore --staged <file>..." to unstage)

--- polygon/prune.sh
+++ polygon/prune.sh
@@ -1,4 +1,4 @@
 
 docker rm -f bor-prune || true
-docker run -d --name bor-prune -v /data/polygon/bor-home:/root/.bor 0xpolygon/bor:0.4.0-amd64 snapshot prune-state --datadir /root/.bor
+docker run -d --name bor-prune -v /data/polygon/bor-home:/root/.bor -v /data/polygon/ancient:/root/.bor/bor/chaindata/ancient 0xpolygon/bor:1.5.3 snapshot prune-state --datadir /root/.bor
 dockerf bor-prune
